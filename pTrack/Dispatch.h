c89e1f56b4fd40e7170ba10249485b702deef857
Kelly Muhindi <kmuhindi@jkuat.ac.ke>
2017-07-10 18:33:45 +0300

    Update Dispatch.h

--- pTrack/Dispatch.h
+++ pTrack/Dispatch.h
@@ -16,14 +16,14 @@
 #pragma comment(lib,"Ws2_32.lib") 
 //tell linker that winsock2 library is needed.
 
-//create client socket 
+//create client socket...connected to server
 SOCKET socketConnect(void) {
 	
 	SOCKET sock = INVALID_SOCKET;
 	struct sockaddr_in sockAddr;
 	sockAddr.sin_family = AF_INET;
-	sockAddr.sin_addr.S_un.S_addr = inet_pton(AF_INET,"127.0.0.1",0);
-	sockAddr.sin_port = htons(8080); //glassfish server
+	sockAddr.sin_addr.S_un.S_addr = inet_pton(AF_INET,"41.204.156.99",0);
+	sockAddr.sin_port = htons(8080); 
 
 	if (connect(sock, (struct sockaddr*)(&sockAddr), sizeof(sockAddr) == INVALID_SOCKET)) {
 		printf("Error connecting to socket\nError: %d", WSAGetLastError());
@@ -56,18 +56,10 @@ int initWSA(WSADATA *wsaData){
 	return 0;
 }
 
-void postData(SOCKET sock) {
-	char httpStr[2048];
-	char * host = "127.0.0.1:8080";
-	char  data[] = "?title=How to Get Moving - Mozilla Firefox";
-	//craft HTML query string and put in buffer (httpStr)
-	sprintf_s(httpStr, sizeof(httpStr), "GET /ptrack/trec?title=How to Get Moving - Mozilla Firefox HTTP/1.1\r\n"
-		"Host: %s\r\n"
-		//"Content-type: application/x-www-form-url-encoded\r\n"
-		"Accept-charset: utf-8\r\n"
-		"Content-length: %d\r\n\r\n", host,sizeof(httpStr));
+/*send data to server*/
+void postData(SOCKET sock,char[2048] urlStr) {
+	sprintf_s(httpStr, sizeof(httpStr), urlStr, host,sizeof(httpStr));
 	puts(httpStr);
-
 	int bytesSent = send(sock, httpStr, strlen(httpStr), 0);
 	if (bytesSent == SOCKET_ERROR) {
 		printf("Error sending data: %ld",WSAGetLastError());
